348f0da3708a94be6f090348ec81696698372861
vorwerkc <vorwerk@physik.hu-berlin.de>
2019-10-25 09:23:47 +0200

    Initial Commit

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='phdpy',
+      version='0.1',
+      description='Plotting library for PhD thesis',
+      author='Christian Vorwerk',
+      author_email='vorwerk@physik.hu-berlin.de',
+      packages=['phdpy'])
